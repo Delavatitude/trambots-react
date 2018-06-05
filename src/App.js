4d458f65deec0f522e9d3b5f72ed5658697d5bf3
Louis Delavaux <Louis@MBP-de-Louis.lan>
2018-06-05 17:31:29 +0200

    fix: chatbox

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
 		      pageId="1842256066012403"
 		      appId="1831084163805835"
 		      htmlRef="site"
+		      logged_in_greeting="Hi! Coucou toi 😜"
+  			  logged_out_greeting="Hi! Coucou toi 😜"
 		    />
 		</div>
     );
